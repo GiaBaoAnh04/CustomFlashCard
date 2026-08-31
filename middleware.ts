0b8782fb982cb9ab2e3b7d25293da77eb5a3e6b9
GiaBaoAnh04 <giabaoanh3003@gmail.com>
2026-08-31 11:50:34 +0700

    fix middleware for deploy

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth } = NextAuth(authConfig);
+
+export function middleware(...args: Parameters<typeof auth>) {
+  return auth(...args);
+}
 
 export const config = {
   matcher: ["/decks/:path*", "/api/decks/:path*"],
